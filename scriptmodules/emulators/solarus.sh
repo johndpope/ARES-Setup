68d94ad39cacad33024bd7288a03dc86a45671e4
Eric Renfro <eric_c_renfro@yahoo.com>
2019-08-22 22:48:54 -0700

    update platforms.cfg for solarus and moved solarus from ports to emulators

--- scriptmodules/emulators/solarus.sh
+++ scriptmodules/emulators/solarus.sh
@@ -15,7 +15,7 @@ rp_module_id="solarus"
 rp_module_desc="Solarus - A lightweight, free and open-source game engine for Action-RPGs"
 rp_module_help="Copy your Solarus quests (games) to $romdir/solarus"
 rp_module_licence="GPL3 https://gitlab.com/solarus-games/solarus/raw/dev/license.txt"
-rp_module_section="prt"
+rp_module_section="sa"
 
 function _options_cfg_file_solarus() {
     echo "$configdir/solarus/options.cfg"
