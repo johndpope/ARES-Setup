4b909368ecd2d77e435a0689e72d0918c6e8f29b
sikotik <33843126+sikotik@users.noreply.github.com>
2019-08-29 20:47:26 +0000

    Add files via upload

---
+++ ares_packages.sh
@@ -0,0 +1,77 @@
+#!/usr/bin/env bash
+
+# This file is part of ARES by The RetroArena
+#
+# ARES is the legal property of its developers, whose names are
+# too numerous to list here. Please refer to the COPYRIGHT.md file distributed with this source.
+#
+# See the LICENSE.md file at the top-level directory of this distribution and
+# at https://raw.githubusercontent.com/Retro-Arena/RetroArena-Setup/master/LICENSE.md
+#
+# Core script functionality is based upon The RetroPie Project https://ares.org.uk Script Modules
+#
+
+__version="4.5.1"
+
+[[ "$__debug" -eq 1 ]] && set -x
+
+# main ares install location
+rootdir="/opt/ares"
+
+# if __user is set, try and install for that user, else use SUDO_USER
+if [[ -n "$__user" ]]; then
+    user="$__user"
+    if ! id -u "$__user" &>/dev/null; then
+        echo "User $__user not exist"
+        exit 1
+    fi
+else
+    user="$SUDO_USER"
+    [[ -z "$user" ]] && user="$(id -un)"
+fi
+
+home="$(eval echo ~$user)"
+datadir="$home/ARES"
+biosdir="$datadir/BIOS"
+romdir="$datadir/roms"
+emudir="$rootdir/emulators"
+configdir="$rootdir/configs"
+
+scriptdir="$(dirname "$0")"
+scriptdir="$(cd "$scriptdir" && pwd)"
+
+__logdir="$scriptdir/logs"
+__tmpdir="$scriptdir/tmp"
+__builddir="$__tmpdir/build"
+__swapdir="$__tmpdir"
+
+# check, if sudo is used
+if [[ "$(id -u)" -ne 0 ]]; then
+    echo "Script must be run under sudo from the user you want to install for. Try 'sudo $0'"
+    exit 1
+fi
+
+__backtitle="https://odroidarena.com/ - ARES Setup. Installation folder: $rootdir for user $user"
+
+source "$scriptdir/scriptmodules/system.sh"
+source "$scriptdir/scriptmodules/helpers.sh"
+source "$scriptdir/scriptmodules/inifuncs.sh"
+source "$scriptdir/scriptmodules/packages.sh"
+
+setup_env
+
+rp_registerAllModules
+
+ensureFBMode 320 240
+
+rp_ret=0
+if [[ $# -gt 0 ]]; then
+    setupDirectories
+    rp_callModule "$@"
+    rp_ret=$?
+else
+    rp_printUsageinfo
+fi
+
+printMsgs "console" "${__INFMSGS[@]}"
+exit $rp_ret
